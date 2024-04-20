9e5c6004d84976ab07964e5243fa1dba27b0c330
天平 <1229248506@qq.com>
2024-04-20 15:38:47 +0800

    feat: 添加判断字符串是不是整数工具

---
+++ String.d.ts
@@ -0,0 +1,1 @@
+export type { IsIntegerString } from './src/String/IsIntegerString'
---
+++ src/String/IsIntegerString.d.ts
@@ -0,0 +1,17 @@
+type IsDigit<Char extends string> = Char extends '0' | '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9'
+    ? true
+    : false
+
+/**
+ * 判断字符串是不是整数
+ * @example
+ * ```ts
+ * type Test1 = AreAllCharactersNumbers<'12345'>; // true
+ * type Test2 = AreAllCharactersNumbers<'1234a5'>; // false
+ * ```
+ */
+export type IsIntegerString<S extends string> = S extends `${infer FirstCharacter}${infer Rest}`
+    ? IsDigit<FirstCharacter> extends true
+        ? IsIntegerString<Rest>
+        : false
+    : true
